de01cbeb898635d0cc43c9dc09b3e8098e6469be
ZyronMelancon <altausolaris@gmail.com>
2017-11-29 17:58:01 -0600

    started project, added hookes law implementation

---
+++ Assets/Scripts/HookesLaw.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HookesLaw
+{
+    public class Particle
+    {
+        public Vector3 position;
+        public Vector3 velocity;
+        Vector3 acceleration;
+        public float mass;
+        Vector3 force;
+
+        public Particle(Vector3 pos)
+        {
+            position = pos;
+            velocity = Vector3.zero;
+            acceleration = Vector3.zero;
+            mass = 1;
+            force = Vector3.zero;
+        }
+
+        public void AddForce(Vector3 f)
+        {
+            force += f;
+        }
+
+        // Update is called once per frame
+        public void Update()
+        {
+            acceleration = force / mass * Time.deltaTime;
+            velocity += acceleration * Time.deltaTime;
+            position += velocity * Time.deltaTime;
+        }
+    }
+
+    public class SpringDamper
+    {
+        Particle p1, p2;
+        float Ks;
+        float Lo;
+
+        public SpringDamper(Particle pone, Particle ptwo, float springConstant, float restLength)
+        {
+            p1 = pone;
+            p2 = ptwo;
+            Ks = springConstant;
+            Lo = restLength;
+        }
+
+        public void Zoz()
+        {
+            float distance = Vector3.Magnitude(p1.position - p2.position);
+            Vector3 normaldir = Vector3.Normalize(p1.position - p2.position);
+
+            Vector3 correction = normaldir * (Lo - distance) * Ks;
+
+            p1.AddForce(correction);
+            p2.AddForce(-correction);
+        }
+    }
+}
---
+++ Assets/Scripts/HookesLawBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookesLawBehaviour : MonoBehaviour {
+
+    public GameObject part1gm;
+    public GameObject part2gm;
+    
+    HookesLaw.SpringDamper dampener;
+    HookesLaw.Particle part1;
+    HookesLaw.Particle part2;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        part1 = new HookesLaw.Particle(part1gm.transform.position);
+        part2 = new HookesLaw.Particle(part2gm.transform.position);
+        dampener = new HookesLaw.SpringDamper(part1, part2, 1, 0.1f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        dampener.Zoz();
+        part1.Update();
+        part2.Update();
+
+        part1gm.transform.position = part1.position;
+        part2gm.transform.position = part2.position;
+    }
+}
